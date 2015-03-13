7a8b32ded668a1afc9188c3d08246a38066de3a5
Phil Martel <pmartel@alum.mit.edu>
2015-03-13 19:17:33 -0400

    initial commit
    CommandOutions.txt is the output of python -h
    run.py is a console program that will run from Idle, from the command
    line, and by double clicking from windows explorer

---
+++ run.py
@@ -0,0 +1,10 @@
+""" working on getting a Python program that will run under Windows without
+calling Python explicitly"""
+# see https://docs.python.org/2/faq/windows.html
+# see http://www.py2exe.org/
+
+""" this runs from the command line, and from Idle """
+print('Hello, Python')
+""" A way of previenting exiting is necessary to see that something is
+    happening if you jus tdouble click from Windows Explorer """
+print(input('?'))

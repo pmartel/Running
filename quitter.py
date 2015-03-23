7e710cff1b8ca4ca1454d23cae67c4e5fa0f163f
Phil Martel <pmartel@alum.mit.edu>
2015-03-22 20:15:16 -0400

    Got it working from command line
    I needed to set up environment variable PYTHONCASEOK (=1) to get around
    a directory name with the wrong case it was looking for
    C:\\Python34\\lib and the path is C:\\Python34\\Lib

--- quitter.py
+++ quitter.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python 
 """ this comes from tkinter.pdf  It runs in IDLE, but gives errror messages
 when run from the command line """
+''' if the environmant variable PYTHONCASEOK is set, this will run from the
+command line.  The problem was that sys.path was
+['', 'C:\\Windows\\system32\\python34.zip', 'C:\\Python34\\DLLs',
+ 'C:\\Python34\\lib', 'C:\\Python34', 'C:\\Python34\\lib\\site-packages']
+ and that the lib directory is C:\Python34\Lib (with a "L")
+'''
 from tkinter import *
+
 class Application(Frame): 
     def __init__(self, master=None):
         Frame.__init__(self, master) 
